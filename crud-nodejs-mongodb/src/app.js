e702f465dc3fe9b51f4570b8265b09cbb41c7f05
Duvan sgf <damejia.98@hotmail.com>
2019-03-02 20:46:43 -0500

    Mongo Proyect End

---
+++ crud-nodejs-mongodb/src/app.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Conecting to BD
+mongoose.connect('mongodb://localhost/crud-mongo')
+ .then(db => console.log('Succesfull conection to DB'))
+ .catch(err => console.log(err));
+
+// importing routes
+const indexRoutes = require('./routes/index');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+app.use('/', indexRoutes);
+// Starting server
+app.listen(app.get('port'), () => {
+    console.log('server on port 3000');
+});
---
+++ crud-nodejs-mongodb/src/routes/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+
+const Task = require('../models/task');
+
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.render('index', {
+        tasks
+    });
+    res.end();
+});
+
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.redirect('/');
+});
+
+router.get('/status/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
+
+router.get('/update/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.render('update', {
+        task
+});
+
+router.post('/update/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.update({_id:id}, req.body);
+    res.redirect('/');
+});
+    
+});
+
+router.get('/delete/:id', async (req, res) =>{
+    const { id } = req.params;
+    await Task.remove({_id: id});
+    res.redirect('/');
+});
+
+module.exports = router;
